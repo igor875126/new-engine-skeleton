f7c4ba696fd8eeefb36f9bb79c700649b6e8171b
Igor Kov <igor875126@gmail.com>
2022-12-01 18:29:37 +0600

    Text object example updated to version 2.0.0 of new-engine

--- src/Objects/Text.ts
+++ src/Objects/Text.ts
@@ -5,11 +5,13 @@ import Vector2 from "new-engine/build/Engine/Vector2";
 import TextObject from "new-engine/build/Objects/TextObject";
 
 export default class Text extends TextObject {
+
     public name: string = 'MyCustomTextObject';
     public text: string = '';
-    public font: string = '20px UpheavalPro';
+    public fontName: string = 'UpheavalPro';
+    public fontSize: number = 25;
     public color: Color = new Color(255, 128, 75, 0);
-    public position: Vector2 = new Vector2(this.core.canvas.width / 2 - 25, this.core.canvas.height / 2 - 50);
+    public position: Vector2 = new Vector2(this.core.canvas.width / 2, this.core.canvas.height / 2 - 50);
     public collider: RectCollider | CircleCollider | null = null;
 
     /**
